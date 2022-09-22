0246a2f2829fbb9d05ab3272d2b11b2416f23950
ntnx0246 <97996034+ntnx0246@users.noreply.github.com>
2022-09-21 23:34:39 -0700

    limelight

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Limelight extends SubsystemBase {
+  /** Creates a new Limelight. */
+  NetworkTableEntry tx; 
+  NetworkTableEntry ty; 
+  NetworkTableEntry ta; 
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  public Limelight() {
+  tx = table.getEntry("tx");
+  ty = table.getEntry("ty");
+  ta = table.getEntry("ta");
+  }
+
+
+  public void getValues(){
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
