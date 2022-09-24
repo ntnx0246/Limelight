41893a9e08b38f40b5a4dde4263ea86585e52e3e
ntnx0246 <97996034+ntnx0246@users.noreply.github.com>
2022-09-24 08:24:20 -0700

    added drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,26 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final static class ID {
+        public static final int DRIVETRAIN_FRONT_LEFT = 2;
+        public static final int DRIVETRAIN_BACK_LEFT = 3;
+        public static final int DRIVETRAIN_FRONT_RIGHT = 0;
+        public static final int DRIVETRAIN_BACK_RIGHT = 1;
+    }
+
+
+    public static final double SLOW_MODE = 0.3;
+    public static final double ERROR_LIMELIGHT = 1;
+
+
+
+    public final static class DualJoystick {
+        public static final int X_AXIS = 0;
+        public static final int Y_AXIS = 1;
+        public static final int DIAL = 2;
+        public static final double DEADZONE = 0.2;
+
+    }
+
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.commands.GetLimelight;
+import frc.robot.commands.Target;
 import frc.robot.subsystems.Limelight;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,7 +22,10 @@ import frc.robot.subsystems.Limelight;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Limelight limelightsubsystem = new Limelight();
+  private final Drivetrain driveTrain = new Drivetrain();
+  private final LogitechGamingPad drivePad = new LogitechGamingPad(0);
 
+  private final JoystickButton buttonA = new JoystickButton(drivePad, 1);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     limelightsubsystem.setDefaultCommand(new GetLimelight(limelightsubsystem));
@@ -32,7 +39,9 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    buttonA.whileHeld(new Target());
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
